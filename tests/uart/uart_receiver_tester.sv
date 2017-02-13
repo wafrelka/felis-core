a1d0a35254f62d90c4f2b2552b1f95a59be7e5e9
wafrelka <wafrelka@gmail.com>
2017-02-13 21:34:07 +0900

    Uart: Fix bit descriptions in receiver test

--- tests/uart/uart_receiver_tester.sv
+++ tests/uart/uart_receiver_tester.sv
@@ -35,7 +35,7 @@ module UartReceiverTester();
 		stop(4);
 		assert(waiting == 1 && ok == 0);
 
-		uart_rx = 0; // starting bit
+		uart_rx = 0; // start bit
 		stop(1);
 		assert(waiting == 0 && ok == 0);
 		stop(4);
@@ -57,13 +57,13 @@ module UartReceiverTester();
 		assert(waiting == 0 && ok == 0);
 		stop(2);
 		assert(waiting == 0 && ok == 0);
-		uart_rx = 1; stop(8); // ending bit
+		uart_rx = 1; stop(8); // stop bit
 		assert(waiting == 1 && ok == 0);
 
 		stop(7);
-		uart_rx = 0; stop(8);
+		uart_rx = 0; stop(8); // start bit
 
-		uart_rx = 1; stop(8);
+		uart_rx = 1; stop(8); // bit 0
 		uart_rx = 1; stop(8);
 		uart_rx = 1; stop(8);
 		uart_rx = 0; stop(8);
@@ -75,7 +75,7 @@ module UartReceiverTester();
 		assert(data == 8'b10000111);
 
 		stop(3);
-		uart_rx = 1; stop(8);
+		uart_rx = 1; stop(8); // stop bit
 		assert(waiting == 1);
 
 		$finish();
