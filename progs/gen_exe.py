43fc766dcf453b5b3c041602e89059fde30bb07d
wafrelka <wafrelka@gmail.com>
2017-02-15 00:34:19 +0900

    Add size data attached sample programs

---
+++ progs/gen_exe.py
@@ -0,0 +1,12 @@
+import subprocess, os
+
+if __name__ == '__main__':
+
+    srcs = ['fib/fib9.bin', 'loopback/loopback.bin', 'man_easy/man_easy.bin']
+
+    for src in srcs:
+
+        fname = os.path.basename(src)
+        dest = os.path.join('exe', fname + '.exe')
+
+        subprocess.call(['python', 'push.py', src, dest])
---
+++ progs/push.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys, os, struct
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 3:
+        print("usage: ./push.py <binfile_path> <uart_path>")
+        exit(1)
+
+    bin_path = sys.argv[1]
+    uart_path = sys.argv[2]
+
+    size = os.stat(bin_path).st_size
+
+    print("from: %s" % bin_path)
+    print("to  : %s" % uart_path)
+    print("size: %d" % size)
+
+    with open(uart_path, "wb") as uart:
+        uart.write(struct.pack("<I", size))
+        with open(bin_path, "rb") as binary:
+            uart.write(binary.read())
