09219ff3a8655e2edacbf89dd5e5d1bef7bb59d7
wafrelka <wafrelka@gmail.com>
2017-02-15 05:51:30 +0900

    Uart/Controller: Add tests for simple transmitting

--- tests/uart/uart_controller_tester.sv
+++ tests/uart/uart_controller_tester.sv
@@ -10,7 +10,7 @@ module UartControllerTester();
 	logic recv_ok;
 
 	logic trans_reset;
-	logic trans_data;
+	logic[7:0] trans_data;
 	logic trans_ok;
 	logic trans_busy;
 
@@ -100,12 +100,50 @@ module UartControllerTester();
 
 		// transmitting data
 
-		uart_in_data = 8'b01011010; uart_in_valid = 1; stop(1);
-		uart_in_data = 8'b01011010; uart_in_valid = 1; stop(1);
+		trans_busy = 1;
+		stop(1);
 
-		// TODO
+		uart_in_data = 8'b01011010; uart_in_valid = 1; stop(1);
+		assert(uart_in_ready == 1); stop(1);
+		assert(uart_in_ready == 0);
+		uart_in_data = 8'b10100101; uart_in_valid = 1; stop(1);
+		assert(uart_in_ready == 1); stop(1);
+		assert(uart_in_ready == 0);
+		uart_in_data = 8'b11100011; uart_in_valid = 1; stop(1);
+		assert(uart_in_ready == 1); stop(1);
+		assert(uart_in_ready == 0);
+		uart_in_data = 8'b00011100; uart_in_valid = 1; stop(1);
+		assert(uart_in_ready == 1); uart_in_valid = 0; stop(1);
+
+		trans_busy = 0;
+		stop(1);
+		assert(trans_ok == 1 && trans_data == 8'b01011010); stop(1);
+		assert(trans_ok == 1 && trans_data == 8'b10100101); stop(1);
+		assert(trans_ok == 1 && trans_data == 8'b11100011); stop(1);
+		assert(trans_ok == 1 && trans_data == 8'b00011100); stop(1);
 
 		// TODO: transmitting data (overflow)
+
+		trans_busy = 1;
+		stop(1);
+
+		uart_in_data = 8'b01011010; uart_in_valid = 1; stop(2);
+		uart_in_data = 8'b10100101; uart_in_valid = 1; stop(2);
+		uart_in_data = 8'b11100011; uart_in_valid = 1; stop(2);
+		uart_in_data = 8'b00011100; uart_in_valid = 1; stop(2);
+		uart_in_data = 8'b10100101; uart_in_valid = 1; stop(2);
+		uart_in_data = 8'b11100011; uart_in_valid = 1; stop(2);
+		uart_in_data = 8'b00011100; uart_in_valid = 1; stop(1);
+		assert(uart_in_ready == 1); stop(1);
+		assert(uart_in_ready == 0); stop(1);
+		uart_in_data = 8'b11111111; uart_in_valid = 1; stop(1);
+		assert(uart_in_ready == 0); stop(1);
+		assert(uart_in_ready == 0);
+
+		trans_busy = 0; stop(1);
+		assert(trans_ok == 1 && trans_data == 8'b01011010);
+		trans_busy = 1; stop(1);
+		assert(uart_in_ready == 1);
 
 		// TODO: simultaneous events (recv_ok and trans_ok)
 
