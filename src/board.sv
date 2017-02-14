e29e76826aefb71d9bc7616ad354616a35a5b0ea
wafrelka <wafrelka@gmail.com>
2017-02-14 23:32:01 +0900

    Board: Add Clock Generator IP

--- src/board.sv
+++ src/board.sv
@@ -1,17 +1,19 @@
 `timescale 1ns / 100ps
 
 module Board(
-	input logic sys_clk,
 	input logic uart_rx,
-	output logic uart_tx
+	output logic uart_tx,
+	input logic clk_p,
+	input logic clk_n
 	);
 
 	logic clk;
 
-	always_comb begin
-		clk = sys_clk;
-	end
+	// clock: 100 [MHz]
+	// baudrate: 115200 [baud]
+	// uart interval: 868 [Hz/baud]
 
 	Chip #(10, 433, 867, 868, 14, 22) chip(.*);
+	ClockIP clock(.clk_in1_p(clk_p), .clk_in1_n(clk_n), .clk_out1(clk));
 
 endmodule
