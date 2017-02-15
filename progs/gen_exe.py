2e45ce923d092839a7919b6e9012e2b33590aa6a
wafrelka <wafrelka@gmail.com>
2017-02-15 21:31:51 +0900

    Add another loopback sample program

--- progs/gen_exe.py
+++ progs/gen_exe.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
     srcs = ['fib/fib9.bin',
         'loopback/loopback.bin',
         'man_easy/man_easy32.bin',
-        'man_easy/man_easy.bin']
+        'man_easy/man_easy.bin',
+        'loopback/hard_loopback.bin']
 
     for src in srcs:
 
---
+++ progs/loopback/hard_loopback.s
@@ -0,0 +1,18 @@
+_min_caml_start:
+in r1
+in r2
+in r3
+in r4
+in r5
+in r6
+in r7
+in r8
+out r1
+out r2
+out r3
+out r4
+out r5
+out r6
+out r7
+out r8
+j _min_caml_start
